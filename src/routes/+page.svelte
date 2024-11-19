f1adeba333a170cbf4f75ca831350d395b383181
tonydiethelm <tonydiethelm@gmail.com>
2024-11-19 10:10:14 -0800

    added basic textarea and button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,10 @@
 <!--
-This is the basic page for the sticky notes application. 
-This needs:
-    An input text bar for which set of sticky notes the user wants to load. 
-    A button with a plus sign to add a new sticky note
-    
-
---------------------------------------------
-                |input|                  [+]
-
-
-        -----------
-           notes
-          blah
-        -----------       ---------
-                            other
-                           notes
-                          ---------
-
-
---------------------------------------------
-
-
-
-When the user writes in a name in the input bar...
-    hit the DB (via the BE) for a set of sticky notes with that name. 
-    It should deliver an object that is a set of sticky note objects.
-    Go through the object and render each sticky note object to the screen in the appropriate location.
-
-When the user hits the plus button, create a new sticky note on the screen. 
-
-
-When the user clicks on a sticky note, they can write on it. 
-
-When the user clicks and holds ona sticky note, they can drag it. 
-
-How to delete?
-    Highlight the sticky note (or multiple notes) and press Delete
-
+See the README for important notes
 -->
 
+
+<script>
+</script>
+
+<textarea placeholder='input the name of your sticky note board here' rows=1 cols='50' ></textarea>
+<button>+</button>
