4a40ea8cc7444645620677c94ffa8819128e9f3c
tonydiethelm <tonydiethelm@gmail.com>
2025-10-08 16:27:53 -0700

    starting work on server.js and page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,117 @@
 <script>
     //stickies are loaded from the 
-    let { data, form } = $props();
+    let { data } = $props();
 
     //imports
 	import Sticky from "../lib/sticky.svelte";
 
-    //I'll need identifiers for the sticky set, to pass to form functions. 
-    //form.setName and form.whoDoesThisBelongTo
+    //set up state.
+    let currentStickySet = $state({})
+    //I need to know what's aleady been posted for comparison so I don't send a set that isn't new. 
+    let alreadyPostedStickySet;
 
-    //set up state. Which I guess is just the form data? Hmm... 
+
+    //I need to keep track of the wantedSticky input and the PeopleAllowed input.
+    let wantedStickyInput = $state('development');
+    let peopleAllowedInput = $state('');
 
 
     //functions
+
+    /*
+    Just a console log to make sure stuff is firing off. 
+    */
     function placeholderFunction(){
-        console.log('the button clicked.');
-        //alert('the button clicked.');
+        console.log('placeholder Function ran.');
     }
 
+    /*
+    Fetch the sticky set
+    Input: user email and desired sticky set name
+    Side Effects: do a GET to the API, stick the return data into currentStickySet/state.
+    return: nada
+    */
+    function getTheStickySet(){
+        console.log("getTheStickySet fired off. Get", wantedStickyInput);
+    }
+
+    /*
+    POST the sticky set
+    Input: the sticky set object
+    Side Effects: put current sticky set object into AlreadyPostedStickySet, do a POST to the API
+    return: nada
+    */
+    function postTheStickySet(){
+        console.log("postTheStickySet fired off");
+
+    }
+
+    /*
+    Post the sticky set if currentStickySet is different than AlreadyPostedStickySet
+    Input: currentStickySet, AlreadyPostedStickySet
+    Side Effects: 
+    */
+    function postIfNewSet() {
+        console.log("do stuff if ", currentStickySet, "and", alreadyPostedStickySet, "are different.");
+        if(currentStickySet != alreadyPostedStickySet){
+            placeholderFunction()
+        };
+        return null;
+    };
+    /*
+    Create new sticky in the sticky set.
+    Input: nothing
+    Side Effects: add a sticky note to the currentStickySet
+    return: nada
+    */
+    function createNewSticky(){
+
+    }
+
+
+    /*
+    Call a timer every X second to run postIfNewSet. set high in testing. 
+    */
+    //const intervalID = setInterval(postIfNewSet, 10000);
+
+
 </script>
-
-<Sticky/>
-<Sticky/>
-
-<Sticky/>
 
 
 <!--
 I need an input and a Get It button for fetching sticky sets. This will need to send the whoDoesThisBelongTo and setName.
 I need a + button to create a new sticky on a given set. This will need to send the whoDoesThisBelongTo and setName.
 I need a log in button that will do OAuth to google to get them an identity to save sticky sets under. 
+I need an input for emails that are allowed to see the set. 
+I need instructions in the background of the center screen. 
 -->
 
 
-<!--
-If there's data, but no form data, display a little howto?-->
+<div id="instructions">
+    <ul>
+        <li>sign in by clicking the little face button on the upper right.</li>
+        <li>type the name of your sticky notes set and hit "get my sticky notes" and your sticky notes will appear.</li>
+        <li>hit the "add sticky" button to add a new sticky note.</li>
+        <li>type in it, drag it whereever you want, no worries.</li>
+        <li>you can type the emails of people you want to share this sticky note set with in the input field under the little face button.</li>
+        <li>Everything auto saves every second, no worries.</li>
 
-<!--
-If there's form data, display a sticky for each note in the form data.
--->
+    </ul>
+</div>
+
+<div id="login">
+
+</div>
+
+<div id="getStickies">
+    <input type='text' bind:value={wantedStickyInput}>
+    <button onclick={getTheStickySet}>Get the sticky set</button>
+</div>
+
+<div id="New Sticky">
+    <button onclick={createNewSticky}>New Sticky</button>
+</div>
+
 
 
 <style>
@@ -45,5 +119,13 @@ If there's form data, display a sticky for each note in the form data.
 I need to style the Get It UI to be top of the page, centered, and always on top. 
 I need to put the log in button on top right. 
 I need to put the + button.... Where? 
+center the instructions div in the center of the screen.
 */
+
+#instructions {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: -1;
+}
 </style>
--- src/routes/+server.js
+++ src/routes/+server.js
@@ -1,4 +1,20 @@
 /** @type {import('./$types').RequestHandler} */
+
+/*
+I need to have a GET and a POST here. 
+
+GET
+Input: user's email and requested sticky set
+Side Effects: if the set doesn't exist, create a new set.
+Return: the requested sticky set object.
+
+POST
+INPUT: the sticky set object
+Side Effects: if they're not allowed to access that, reject it. If they are, save it. 
+Return: success or fail
+*/
+
+
 export async function GET() {
     return new Response();
 };
